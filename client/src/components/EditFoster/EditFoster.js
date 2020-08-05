ebdd7e6e5f70f070fa2c0c95c61a3f0fa86aae8e
Christian Escobar <crs.escobar@gmail.com>
2020-08-04 21:37:44 -0700

    handle edits

--- client/src/components/EditFoster/EditFoster.js
+++ client/src/components/EditFoster/EditFoster.js
@@ -6,42 +6,109 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+	MuiPickersUtilsProvider,
+	KeyboardDatePicker,
+} from '@material-ui/pickers';
+import moment from 'moment';
 
 const EditFoster = (props) => {
-	const [open, setOpen] = React.useState(true);
+	const [receivedDate, setReceivedDate] = React.useState(props.received);
+	const [adoptedDate, setAdoptedDate] = React.useState(props.adopted);
+	const [adoptionAgency, setAdoptionAgency] = React.useState(props.agency);
 
 	const handleClose = () => {
-		setOpen(false);
+		props.setEdit(false);
+	};
+
+	const handleSubmit = async () => {
+		const body = {
+			Key: props.awskey,
+			adoptionAgency,
+			receivedDate,
+			adoptedDate,
+		};
+
+		await fetch('/api/v1/fosters/update', {
+			method: 'PUT',
+			body: JSON.stringify(body),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		props.setEdit(false);
+	};
+
+	const handleReceivedDateChange = (date) => {
+		setReceivedDate(moment(date).format('MM/DD/YYYY'));
+	};
+
+	const handleAdoptedDateChange = (date) => {
+		setAdoptedDate(moment(date).format('MM/DD/YYYY'));
+	};
+
+	const handleAdoptionAgencyChange = (event) => {
+		setAdoptionAgency(event.target.value);
 	};
 
 	return (
 		<div>
 			<Dialog
-				open={open}
+				open={props.showDialog}
 				onClose={handleClose}
 				aria-labelledby="form-dialog-title"
 			>
-				<DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+				<DialogTitle id="form-dialog-title">Edit Details</DialogTitle>
 				<DialogContent>
 					<DialogContentText>
-						To subscribe to this website, please enter your email address here.
-						We will send updates occasionally.
+						Update stored details for {props.fosterName}
 					</DialogContentText>
 					<TextField
 						autoFocus
 						margin="dense"
-						id="name"
-						label="Email Address"
-						type="email"
+						id="agency"
+						label="Adoption Agency"
+						value={adoptionAgency}
+						onChange={handleAdoptionAgencyChange}
+						type="text"
 						fullWidth
 					/>
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<KeyboardDatePicker
+							margin="normal"
+							fullWidth
+							id="received-date"
+							name="received-date"
+							label="Received"
+							value={receivedDate}
+							onChange={handleReceivedDateChange}
+							format="MM/dd/yyyy"
+							KeyboardButtonProps={{
+								'aria-label': 'change date',
+							}}
+						/>
+					</MuiPickersUtilsProvider>
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<KeyboardDatePicker
+							margin="normal"
+							fullWidth
+							id="adopted-date"
+							name="adopted-date"
+							label="Adopted"
+							value={adoptedDate}
+							onChange={handleAdoptedDateChange}
+							format="MM/dd/yyyy"
+							KeyboardButtonProps={{
+								'aria-label': 'change date',
+							}}
+						/>
+					</MuiPickersUtilsProvider>
 				</DialogContent>
 				<DialogActions>
 					<Button onClick={handleClose} color="primary">
 						Cancel
 					</Button>
-					<Button onClick={handleClose} color="primary">
-						Subscribe
+					<Button onClick={handleSubmit} color="primary">
+						Submit
 					</Button>
 				</DialogActions>
 			</Dialog>
