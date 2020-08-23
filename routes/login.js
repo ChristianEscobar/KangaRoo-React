ec1228e77f46d182d91c3c68ffd73be845478557
Christian Escobar <crs.escobar@gmail.com>
2020-08-23 16:58:23 -0700

    remove unnecessary code

--- routes/login.js
+++ routes/login.js
@@ -2,31 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const authRouter = function (passport) {
-	// router.post('/login', (req, res, next) => {
-	// 	passport.authenticate('local', (err, user, info) => {
-	// 		if (err) {
-	// 			console.log(err);
-	// 			throw err;
-	// 		}
-
-	// 		if (!user) {
-	// 			console.log('no user');
-	// 			res.send('User does not exist');
-	// 		} else {
-	// 			req.logIn(user, (err) => {
-	// 				if (err) {
-	// 					console.log(err);
-	// 					throw err;
-	// 				}
-
-	// 				console.log('success');
-	// 				res.send('Successfully Authenticated');
-	// 				console.log(req.user);
-	// 			});
-	// 		}
-	// 	});
-	// });
-
 	router.post(
 		'/login',
 		passport.authenticate('local', {
